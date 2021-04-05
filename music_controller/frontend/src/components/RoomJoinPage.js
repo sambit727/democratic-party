fe6e9a21cdc62e959e70017743b88668b34ed3eb
Sambit <sambit.basu.15@ucl.ac.uk>
2021-04-05 18:12:21 +0100

    05.04.2021 changes

---
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+export default class RoomJoinPage extends Component {
+  defaultVotes = 2;
+
+    constructor(props) {
+      super(props);
+      }
+    
+    render() {
+        return <h2>Room Join Page</h2>
+      }
+    }
